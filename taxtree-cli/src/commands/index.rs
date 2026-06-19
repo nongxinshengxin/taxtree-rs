650cae03b13ff8a65ff01010c4ea5d4cde845020
nongxinshengxin <124514608+nongxinshengxin@users.noreply.github.com>
2026-06-19 15:04:25 +0800

    Delete taxtree-cli directory

--- taxtree-cli/src/commands/index.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-use taxtree_core::{TaxTreeError, TaxonomyIndex};
-
-pub fn run(dump_dir: &Path, out: &Path) -> Result<(), TaxTreeError> {
-    let index = TaxonomyIndex::build_from_dump(dump_dir)?;
-    index.save(out)?;
-    Ok(())
-}
